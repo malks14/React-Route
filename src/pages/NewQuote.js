a66d3fdef8522917918d70e025ee97f793522a63
Guido Greco <guidogreco@Guidos-MacBook-Pro.local>
2021-11-01 14:43:30 -0300

    new commit

---
+++ src/pages/NewQuote.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import useHttp from '../hooks/use-http'
+import QuoteForm from '../components/quotes/QuoteForm';
+import { addQuote } from '../lib/api';
+
+
+const NewQuote = () => {
+    const history = useHistory();
+    const {sendRequest, status} =  useHttp(addQuote);
+
+    useEffect(() => {
+        if (status === 'completed') {
+            history.push('/quotes');
+        }
+    }, [status, history]);
+
+    const addQuoteHandler = quoteData => {
+        sendRequest(quoteData)
+
+    };
+
+    return <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler}/>
+};
+
+
+export default NewQuote;
